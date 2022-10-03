606f5ac5ad97fdebb42c51551a0327e604e6910c
shourja ganguly <shourja.ganguly@konnectbox.in>
2022-10-03 20:45:01 +0530

    abscasc

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const mongo = require('./Shared/mongo');
 const students = require('./Routes/student.routes');
 const mentors= require('./Routes/mentors.routes');
 const cors=require('cors');
-const routes = require('./Routes/mentors.routes');
+const dotenv = require('dotenv');
+
+
 (async () => {
     try {
         await mongo.connect();
+        dotenv.config();
         app.use(cors());
         app.use(express.json());
         app.get("/",()=>res.send("Running"))
         app.use("/",students);
         app.use("/",mentors);
-        app.listen(5001, () => console.log('listening on port 5001'));
+        app.listen(process.env.PORT, () => console.log('listening on port',process.env.PORT));
     }
     catch (e) {
         console.log(e);
