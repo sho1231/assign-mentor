1a920b9f615b2a874ede86d9b5ac9a5fb18ebc09
shourja ganguly <shourja.ganguly@konnectbox.in>
2022-10-03 20:36:40 +0530

    deployment

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const mongo = require('./Shared/mongo');
 const students = require('./Routes/student.routes');
 const mentors= require('./Routes/mentors.routes');
+const cors=require('cors');
 (async () => {
     try {
         await mongo.connect();
+        app.use(cors());
         app.use(express.json());
         app.use("/",students);
         app.use("/",mentors);
